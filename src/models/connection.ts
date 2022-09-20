44cae46795f966ad9638538d5ce8ea3089fe0d8f
Jonathan Santos <jonathan01347@gmail.com>
2022-09-20 13:08:53 -0300

    chore(add package-lock/rm anotações): adicionado arquivo package-lock.json e removido comentarios.

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,6 +7,5 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-}); // sua conexão NÃO deve ter o database, este deve ser especificado em cada query
-
+});
 export default connection;
